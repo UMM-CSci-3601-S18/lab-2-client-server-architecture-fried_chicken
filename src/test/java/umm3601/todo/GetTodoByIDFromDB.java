e2d2a2c16a38016b9cbe66b531975ad4db29a080
rocherno <dejon076@morris.umn.edu>
2018-02-10 18:22:21 -0600

    Add tests for method getTodo. Tests passed successfully

---
+++ src/test/java/umm3601/todo/GetTodoByIDFromDB.java
@@ -0,0 +1,27 @@
+package umm3601.todo;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Tests umm3601.user.Database getUser functionality
+ */
+public class GetTodoByIDFromDB {
+
+  @Test
+  public void getBlanche() throws IOException {
+    Database db = new Database("src/main/data/todos.json");
+    Todo todo = db.getTodo("58895985a22c04e761776d54");
+    assertEquals("Incorrect name", "Blanche", todo.owner);
+  }
+
+  @Test
+  public void getFry() throws IOException {
+    Database db = new Database("src/main/data/todos.json");
+    Todo todo = db.getTodo("58895985c1849992336c219b");
+    assertEquals("Incorrect name", "Fry", todo.owner);
+  }
+}
