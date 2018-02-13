eb43ba312076a87e19bbe4031f041d5c13073abf
rocherno <dejon076@morris.umn.edu>
2018-02-12 19:18:29 -0600

    issue#11 Add Todo constructor. Add Tests for orderBYOwner. Tests pass.

--- src/main/java/umm3601/todo/Todo.java
+++ src/main/java/umm3601/todo/Todo.java
@@ -7,4 +7,16 @@ public class Todo{
   String body;
   String category;
 
+  public Todo(String _id, String owner, Boolean status, String body, String category){
+    this._id = _id;
+    this.owner =owner;
+    this.status = status;
+    this.body = body;
+    this.category = category;
+  }
+
+  public String toString() {
+    return String.format(_id + " " + owner + " " + status + " " + body + " " + category);
+  }
+
 }
--- src/test/java/umm3601/todo/SortTodosAlphabeticallyByFieldFromDB.java
+++ src/test/java/umm3601/todo/SortTodosAlphabeticallyByFieldFromDB.java
@@ -4,26 +4,51 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
+
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 public class SortTodosAlphabeticallyByFieldFromDB {
-  ArrayList <Todo> todos = new ArrayList<>();
 
   @Test
-  public void filterTodosByBody() throws IOException {
+  public void filterTodosByOwner() throws IOException {
     Database db = new Database("src/main/data/todos.json");
     Todo[] allTodos = db.listTodos(new HashMap<>());
 
-    Todo[] Todos = db.orderTodosByField(allTodos, "owner");
-    for(int i = 0; i < 300; i++){
-      System.out.println(Todos[i].owner);
-    }
-    assertEquals("Incorrect number of todos with enim", 1, Todos.length);
+    Todo[] arr1 = new Todo[3];
+    arr1[0] = new Todo("9119111911", "KyleFoss",false,"slim with glasses", "student");
+    arr1[1] = new Todo("9119111912", "RochSexy",true,"in shape", "Chef");
+    arr1[2] = new Todo("9119111913", "AndyLau",false,"not slim with glasses", "lazyner");
 
-    Todo[] Todos2 = db.filterTodosByBody(allTodos, "Roch");
-    assertEquals("Incorrect number of todos with banana", 0, Todos2.length);
+    Todo[] arr2 = new Todo[3];
+    arr2[1] = new Todo("9119111911", "KyleFoss",false,"slim with glasses", "student");
+    arr2[2] = new Todo("9119111912", "RochSexy",true,"in shape", "Chef");
+    arr2[0] = new Todo("9119111913", "AndyLau",false,"not slim with glasses", "lazyner");
+
+    Todo[] Todos = db.orderTodosByField(arr1, "owner");
+
+    for(int i = 0; i < 1; i++){
+      assertEquals("They don't equal", 0, arr1[i].owner.compareTo(arr2[i].owner));
+    }
+  }
+
+  @Test
+  public void listTodosWithByOwnerSorted() throws IOException {
+    Database db = new Database("src/main/data/todos.json");
+    Map<String, String[]> queryParams = new HashMap<>();
+
+    queryParams.put("orderBy", new String[] {"owner"});
+    Todo[] todos1 = db.listTodos(queryParams);
+    Todo[] todos2 = db.orderTodosByField(todos1, "owner");
+    System.out.println();
+    for(int i =0; i < todos1.length-1; i++) {
+    assertEquals("Incorrect number of todos with enim", true, todos2[i].owner.compareTo(todos2[i+1].owner) <= 0);
+    }
+
   }
 
 }
